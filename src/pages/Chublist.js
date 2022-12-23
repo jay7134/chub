8abc016aae0bab245bf13b3ea8305af12788f83b
Javed <jay7134@gmail.com>
2022-12-23 10:46:58 +0500

    Update Chublist.js

--- src/pages/Chublist.js
+++ src/pages/Chublist.js
@@ -10,7 +10,7 @@ function Chubslist() {
         <h2>Click the Chublist for play the game</h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             
-            <img src='assets/images/Chub-run.png' className="img-fluid mx-auto d-block" alt="Chub Run" width={484} height={484}/>
+            <img src='assets/images/chub-run.png' className="img-fluid mx-auto d-block" alt="Chub Run" width={484} height={484}/>
         </div>
       
         <div className='offset-md-2 col-md-4 col-sm-6' data-aos="fade-up" data-aos-duration="1000">
@@ -18,7 +18,7 @@ function Chubslist() {
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             <div className='mz-spacing'></div>
             <Link to="/code">
-            <img src='assets/images/Chub-code.png' className="img-fluid mx-auto d-block" alt="Chub Code" width={559} height={419}/>
+            <img src='assets/images/chub-code.png' className="img-fluid mx-auto d-block" alt="Chub Code" width={559} height={419}/>
             </Link>
         </div>
       </div>
